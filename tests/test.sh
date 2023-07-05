52e06726df7939316b6c29b545919d680eebaa9f
andregit <defcklxrd@mail.ru>
2023-07-05 19:03:26 +0000

    adas

---
+++ tests/test.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+sudo apt-get -y install python3
+pip install -r requirements.txt
+python3 hello_app/app.py &
+sleep 5
+ANSW=$(curl http://127.0.0.1:5000/)
+if [ "$ANSW" != "2023-07-05" ]; then
+echo "Neudacha"
+else
+echo "Udacha"
+fi
+
